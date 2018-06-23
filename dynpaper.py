bf6c12729927cf8d8f0e4f908e8478096d223d2f
stelios tymvios <stelios.tymvios@icloud.com>
2018-06-23 10:02:22 +0300

    wip kde

--- dynpaper.py
+++ dynpaper.py
@@ -13,10 +13,22 @@ VERSION = '1.2.2'
 
 
 PROCESS_CALLS = {
-    'gnome': "DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{}",
-    'budgie': "DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{}",
-    'nitrogen': "nitrogen --set-auto {}",
-    'feh': "feh --bg-scale {}",
+    'gnome': "DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{file}",
+    'budgie': "DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{file}",
+    'nitrogen': "nitrogen --set-auto {file}",
+    'feh': "feh --bg-scale {file}",
+    'kde': """
+                qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript '
+                    var allDesktops = desktops();
+                    print (allDesktops);
+                    for (i=0;i<allDesktops.length;i++) {{
+                        d = allDesktops[i];
+                        d.wallpaperPlugin = "org.kde.image";
+                        d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");
+                        d.writeConfig("Image", "file:///{file}")
+                    }}
+                '
+            """,
 }
 
 
